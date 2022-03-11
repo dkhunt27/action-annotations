07f1b79d8c5cb688ea4a0240f760189c744bcf46
Dan Hunt <dkhunt27@gmail.com>
2022-03-11 09:44:01 -0500

    fix: rebuilt

--- dist/index.js
+++ dist/index.js
@@ -77,32 +77,24 @@ var __importStar = (this && this.__importStar) || function (mod) {
     __setModuleDefault(result, mod);
     return result;
 };
-var __awaiter = (this && this.__awaiter) || function (thisArg, _arguments, P, generator) {
-    function adopt(value) { return value instanceof P ? value : new P(function (resolve) { resolve(value); }); }
-    return new (P || (P = Promise))(function (resolve, reject) {
-        function fulfilled(value) { try { step(generator.next(value)); } catch (e) { reject(e); } }
-        function rejected(value) { try { step(generator["throw"](value)); } catch (e) { reject(e); } }
-        function step(result) { result.done ? resolve(result.value) : adopt(result.value).then(fulfilled, rejected); }
-        step((generator = generator.apply(thisArg, _arguments || [])).next());
-    });
-};
 Object.defineProperty(exports, "__esModule", ({ value: true }));
+exports.run = void 0;
 const core = __importStar(__nccwpck_require__(186));
 const annotate_1 = __nccwpck_require__(553);
-function run() {
-    return __awaiter(this, void 0, void 0, function* () {
-        try {
-            const type = core.getInput('type');
-            const msg = core.getInput('msg');
-            (0, annotate_1.annotate)({ type, msg });
-        }
-        catch (error) {
-            if (error instanceof Error)
-                core.setFailed(error.message);
-        }
-    });
-}
-run();
+// exported for testing
+const run = () => {
+    try {
+        const type = core.getInput('type');
+        const msg = core.getInput('msg');
+        (0, annotate_1.annotate)({ type, msg });
+    }
+    catch (error) {
+        if (error instanceof Error)
+            core.setFailed(error.message);
+    }
+};
+exports.run = run;
+(0, exports.run)();
 
 
 /***/ }),
